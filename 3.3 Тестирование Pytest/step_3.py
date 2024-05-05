00d53d168f4f2f81db5457ae4ee1b0c19fe390a0
ne_srv <ne_srv@mail.ru>
2024-05-05 16:21:48 +0300

    autosave

--- 3.3 Тестирование Pytest/step_3.py
+++ 3.3 Тестирование Pytest/step_3.py
@@ -1,31 +1,42 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-try: 
-    link = "http://suninjuly.github.io/registration1.html"
-    # link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)   
-   
+link1 = "http://suninjuly.github.io/registration1.html"
+link2 = "http://suninjuly.github.io/registration2.html"
+text_expected = "Congratulations! You have successfully registered!"
+
+class TestAbs(unittest.TestCase):
     
-    browser.find_element(By.XPATH, "//div[1]/div[1]/input").send_keys('first')
-    browser.find_element(By.XPATH, "//div[1]/div[2]/input").send_keys('last')
-    browser.find_element(By.XPATH, "//div[1]/div[3]/input").send_keys('mail@mail.ru')
-    browser.find_element(By.XPATH, "//div/form/div[2]/div[1]/input").send_keys('89001234567')
-    browser.find_element(By.XPATH, "//div/form/div[2]/div[2]/input").send_keys('address') 
+    @staticmethod
+    def reg_form(link):
+        browser = webdriver.Chrome()
+        browser.get(link) 
+        browser.find_element(By.XPATH, "//div[1]/div[1]/input").send_keys('first')
+        browser.find_element(By.XPATH, "//div[1]/div[2]/input").send_keys('last')
+        browser.find_element(By.XPATH, "//div[1]/div[3]/input").send_keys('mail@mail.ru')
+        browser.find_element(By.XPATH, "//div/form/div[2]/div[1]/input").send_keys('89001234567')
+        browser.find_element(By.XPATH, "//div/form/div[2]/div[2]/input").send_keys('address')
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()     
+        time.sleep(1)
 
+        return browser.find_element(By.TAG_NAME, "h1").text
 
-    browser.find_element(By.CSS_SELECTOR, "button.btn").click() 
+        # .first_block > div:nth-child(1) > input:nth-child(2) 
+    def test_link1(self):
+          self.assertEqual(self.reg_form(link1), text_expected, "Should be expected text")
+        
+    def test_link2(self):
+          self.assertEqual(self.reg_form(link2), text_expected, "Should be expected text")
+            
  
-    time.sleep(1)
-
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-
-    welcome_text = welcome_text_elt.text
-
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    time.sleep(10)
-    browser.quit()
+        
+if __name__ == "__main__":
+    unittest.main()
+    
+    
+# browser.find_element(By.CSS_SELECTOR, ".first_block .first_class input").send_keys("Ivan")
+# browser.find_element(By.CSS_SELECTOR, ".first_block .second_class input").send_keys("Petrov")
+# browser.find_element(By.CSS_SELECTOR, ".first_block .third_class input").send_keys("abc@gmail.com")
+# browser.find_element(By.CSS_SELECTOR, "button.btn").click()
